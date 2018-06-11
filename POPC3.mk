84d88aa6eb43fba22e23ed082445df721e108c77
Manish4586 <manish.n.manish45@gmail.com>
2018-06-12 02:23:52 +0530

    first commit

---
+++ POPC3.mk
@@ -0,0 +1,12 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oysters/t72hm/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oysters/t72hm/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oysters/t72hm/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oysters/t72hm/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oysters/t72hm/proprietary/xbin,system/xbin)
+
